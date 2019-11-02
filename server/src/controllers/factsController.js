5b3da58a13c8b4d7b4f581279ac9bfbfff6bad38
Elisa <elisam.esquivel@gmail.com>
2019-11-01 18:48:05 -0600

    Save random fact in DB

--- server/src/controllers/factsController.js
+++ server/src/controllers/factsController.js
@@ -10,14 +10,14 @@ exports.getFacts = ('/', (req, res) => {
 exports.randomFact = ('/randomFact', (req, res, next) => {
   axios.get('https://uselessfacts.jsph.pl/random.json')
   .then(async ({data}) => {
-    res.status(200).json(data)
     const count = await Fact.countDocuments() + 1
-    const newFact = Fact.create({
+    const newFact = await Fact.create({
       id:count,
       text:data.text,
       source:data.source,
       sourceURL:data.source_url
     })
+    res.status(200).json({newFact})
   })
   .catch(err => console.log(err))
 })
